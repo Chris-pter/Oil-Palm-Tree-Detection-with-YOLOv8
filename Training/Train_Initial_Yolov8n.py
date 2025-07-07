cb4b3320e2c802335850c82fd5eff63af02a88e8
Chris_Pter <christensonpetermail@gmail.com>
2025-07-07 09:33:41 +0800

    Update Train_Initial_Yolov8n.py

--- Training/Train_Initial_Yolov8n.py
+++ Training/Train_Initial_Yolov8n.py
@@ -1,1 +1,17 @@
+#Import the YOLO class from the Ultralytics package
+from ultralytics import YOLO
 
+#Load a pretrained YOLOv8 model
+#Options include: 'Yolov8n.pt', 'Yolov8s.pt', 'Yolov8m.pt', etc.
+model=YOLO("yolov8n.pt") #path/to/your/checkpoint/best.pt 
+#Replace with your own .pt file if resuming training
+
+#Start training the model
+model.train(
+  data="path/to/your/data.yaml",      #Path to your data config YAML file
+  epochs=50,                          #Number of training epochs
+  imgsz=640,                          #Image size (Input resolution)
+  project="path/to/save/the/project", #Output directory for training results
+  name="your_folder_name",            #Folder name inside the project
+  exist_ok=True                       #Overwrite folder if it exist
+)
