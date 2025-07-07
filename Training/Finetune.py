d4aeae083089c9da2b36d8b9746a1bdd75648bdb
Chris_Pter <christensonpetermail@gmail.com>
2025-07-07 09:36:07 +0800

    Update Finetune.py

--- Training/Finetune.py
+++ Training/Finetune.py
@@ -1,1 +1,14 @@
+from ultralytics import YOLO
 
+#Load the best weights from a previous batch
+model=YOLO("yolov8n.pt")
+
+#Fine-tuned using new data or more epochs
+model.train(
+  data="path/to/your/data.yaml",      #Same or updated dataset
+  epochs=50,                          
+  imgsz=640,                          
+  project="path/to/save/the/project", 
+  name="your_folder_name",            
+  exist_ok=True                       
+)
