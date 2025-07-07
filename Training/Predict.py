d7cc9857996a66b3bf7934a4ca202c27e5326569
Chris_Pter <christensonpetermail@gmail.com>
2025-07-07 09:43:59 +0800

    Update Predict.py

--- Training/Predict.py
+++ Training/Predict.py
@@ -1,1 +1,16 @@
+#predict.py
+#Run ibject detection on images, videos, or folders using a trained YOLO model
 
+from ultralytics import YOLO
+
+#Load the pretrained model (.pt)
+model=YOLO("path/to/your/final_model.pt")
+
+#Run prediction
+results=model.predict(  
+  source="path/to/your/image_or_folder",  #e.g., "test.jpg", "images/"
+  conf=0.5,                               #Confidence treshold (adjust as needed)
+  save=True/False,                        #Save the output images with bounding boxes
+  save_txt=True/False,                    #Save True if you want YOLO-format .txt files
+  imgsz=640                               #Images siuze (should match your training size)
+)
